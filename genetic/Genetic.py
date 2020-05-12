30c7740846a9b68e34b016fb2a25530b3a183170
tirgue <55982589+tirgue@users.noreply.github.com>
2020-05-12 11:28:10 +0200

    fix : bread working

--- genetic/Genetic.py
+++ genetic/Genetic.py
@@ -21,19 +21,20 @@ class Genetic(object):
         max = [copy.deepcopy(self.population[0])]
         for i in range(1, nb):
             insert = False
-            for l in range(len(max) - 1, -1, -1):
+            for l in range(0, len(max)):
                 if max[l].fitness < self.population[i].fitness:
-                    max.insert(l + 1, copy.deepcopy(self.population[i]))
+                    max.insert(l, copy.deepcopy(self.population[i]))
                     insert = True
                     break
 
             if not insert:
-                max.insert(0, copy.deepcopy(self.population[i]))
+                max.insert(len(max), copy.deepcopy(self.population[i]))
 
-        for i in range(1, self.nb_population):
-            if self.population[i].fitness > max[nb-1].fitness:
-                max.append(copy.deepcopy(self.population[i]))
-                max.pop(0)
+        for i in range(nb, self.nb_population):
+            for j in range(len(max)):
+                if self.population[i].fitness > max[j].fitness:
+                    max.insert(j, copy.deepcopy(self.population[i]))
+                    max.pop()
 
         return max
 
@@ -42,12 +43,12 @@ class Genetic(object):
             for l in range(1, self.population[p].neuron_network.number_of_hidden_layers + 2):
                 for n in range(self.population[p].neuron_network.layers[l].number_of_neurons):
                     for w in range(self.population[p].neuron_network.layers[l].neurons_weight[n].shape[0]):
-                        percent = random.randrange(1, 100, 1)
+                        percent = random.randrange(1, 101, 1)
                         if percent <= mutation_rate:
                             self.population[p].neuron_network.layers[l].neurons_weight[n][w] = 2 * np.random.random() - 1
                         
                         else:
-                            index = random.randrange(0,len(individuals)-1,1)
+                            index = random.randrange(0,len(individuals),1)
                             self.population[p].neuron_network.layers[l].neurons_weight[n][w] = individuals[index].neuron_network.layers[l].neurons_weight[n][w]
 
                     percent = random.randrange(1, 100, 1)
@@ -55,7 +56,7 @@ class Genetic(object):
                         self.population[p].neuron_network.layers[l].biais = 2 * np.random.random() - 1
                     
                     else:
-                        index = random.randrange(0,len(individuals)-1,1)
+                        index = random.randrange(0,len(individuals),1)
                         self.population[p].neuron_network.layers[l].biais = individuals[index].neuron_network.layers[l].biais
 
             self.population[p].fitness = 0
